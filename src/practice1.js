7fe303cc54be56656250120cd3e03d4ac5fabedf
shj-j <shang.jie@outlook.com>
2020-09-11 18:46:14 +0800

    done practice 1 & 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,9 @@
-const parseData
+const getColumnName = (columns) => (columns.map(column => column.name));
+const getDataValue = (keys, value) => keys.reduce((acc, key, index) => ({
+ ...acc , [key]: value[index]
+}), {})
+const parseData = (res) => {
+  const keys = getColumnName(res.column);
+  return res.data.map(data => getDataValue(keys,data)) 
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+  sections.forEach((element, index) => {
+    items.splice(element.index+index, 0, element.content) 
+  });
+  return items;
+}
 export { inject };
