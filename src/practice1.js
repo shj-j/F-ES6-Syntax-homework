c375945ab30d0e07c92dcfd0fe1eba2376b7569b
kevindongzg <dongkevin94@gmail.com>
2020-09-22 15:37:01 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -2,8 +2,11 @@ const getColumnName = (columns) => (columns.map(column => column.name));
 const getDataValue = (keys, value) => keys.reduce((acc, key, index) => ({
  ...acc , [key]: value[index]
 }), {})
+// TODO 使用解构的方式取出data与column
 const parseData = (res) => {
+  // TODO keys不是很有必要
   const keys = getColumnName(res.column);
+  // TODO reduce掌握不错，parseData最终可以简化成一行
   return res.data.map(data => getDataValue(keys,data)) 
 }
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,4 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
   sections.forEach((element, index) => {
     items.splice(element.index+index, 0, element.content) 
